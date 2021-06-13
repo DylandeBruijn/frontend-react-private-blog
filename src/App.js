fdaaae08b456d8728c9a30cf0e9e3743f0367e30
DylandeBruijn <dylandebruyn1993@gmail.com>
2021-06-13 15:43:48 +0200

    Added functionality to App.js for private blog

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
-import React, { useState } from 'react';
-import './App.css';
+// React
+
+import React, { useState } from "react";
+
+// React Router
+
+import { Switch, Route, Redirect } from "react-router-dom";
+
+// Components
+
+import TopMenu from "./components/TopMenu";
+
+// Pages
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Blog from "./pages/Blog";
+import Blogpost from "./pages/Blogpost";
+
+// Blogposts
+
+import posts from "./data/posts.json";
+
+// CSS
+
+import "./css/styles.css";
 
 function App() {
-  // We houden in de state bij of iemand is "ingelogd" (simpele versie)
-  const [isAuthenticated, toggleIsAuthenticated ] = useState(false);
+	// States
 
-  return (
-    <div>
-      Maak hier jouw prachtige blog-applicatie!
-    </div>
-  );
+	const [isAuthenticated, toggleIsAuthenticated] = useState(false);
+
+	const handleLogin = () => {
+		toggleIsAuthenticated(!isAuthenticated);
+	};
+
+	return (
+		<>
+			<TopMenu isAuthenticated={isAuthenticated} handleLogin={handleLogin} />
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route exact path="/login">
+					{isAuthenticated ? (
+						<Redirect to="/blog" />
+					) : (
+						<Login
+							isAuthenticated={isAuthenticated}
+							handleLogin={handleLogin}
+						/>
+					)}
+				</Route>
+				<Route exact path="/blog">
+					{isAuthenticated ? <Blog posts={posts} /> : <Redirect to="/login" />}
+				</Route>
+				<Route exact path="/blog/:id">
+					{isAuthenticated ? (
+						<Blogpost posts={posts} />
+					) : (
+						<Redirect to="/login" />
+					)}
+				</Route>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
